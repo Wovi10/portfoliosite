0b00ec5bc540c832d952276b7e6f5072571c208a
Wout Vinckevleugel <wout.vinck@gmail.com>
2022-09-13 17:12:45 +0200

    Started writing intro
    Added new css file with text layouts

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -4,9 +4,9 @@ export default function Introduction() {
     return (
         <section id="socials" className="my-5 mx-auto">
             <div className="container flex flex-col">
-                <h2 className="uppercase font-semibold m-auto left-1/2 py-1">
-                    Hello!!!
-                </h2>
+                <div className="title m-auto left-1/2 py-1">
+                    Hello!!
+                </div>
                 <div className="flex mx-auto md:w-1/2">
                     <IntroductionText />
                 </div>
--- src/data/Introduction.tsx
+++ src/data/Introduction.tsx
@@ -1,7 +1,27 @@
+import '../css/textlayout.css'
+
 export default function IntroductionText() {
     return(
-        <div>
-            Welcome to my very own website.
+        <div className="textleft">
+            <div className="subtitle">
+                <strong>Welcome</strong> to my very own website.
+            </div>
+            <div className="paragraph">
+                Here you'll find who I am and what drives me.
+                But also what I did so far and what my future might look like.
+            </div>
+            <div className="subtitle">
+                About me
+            </div>
+            <div className="paragraph">
+                My name is <strong>Wout Vinckevleugel</strong> and I'm a Belgian backend developer.
+                <br/>
+                In 2001 I was born in Antwerp, Belgium.
+                <br/>
+                I speak both <strong>Dutch</strong> and <strong>English</strong> fluently as well as a bit of <strong>French</strong>.
+                
+            </div>
         </div>
+
     );
 }
